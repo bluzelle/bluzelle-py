5bf26fd9faefa81fe25037176792673ec624ee6f
Travis CI <travis@travis-ci.org>
2019-07-24 21:04:05 +0000

    Bump version: 0.67.0 -> 0.68.0 [ci skip]

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ class build_ext(build_ext_orig):
 
 setuptools.setup(
     name="bluzelle",
-    version="0.67.0",
+    version="0.68.0",
     author="Yarco Hayduk",
     author_email="yaroslav@bluzelle.com",
     description="A Python Bluzelle client",
